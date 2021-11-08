3a198f7c0900cf4a3b5480201e96c0584de4976d
almostevil667 <david.schuldes@icloud.com>
2021-11-08 16:43:10 +0100

    added cross-entropy-loss-function

--- FullyConnectedNetwork.py
+++ FullyConnectedNetwork.py
@@ -1,13 +1,33 @@
 import numpy as np
 import random
 
+class QuadraticCost(object):
+    @staticmethod
+    def cost(a,y):
+        return 0.5 * np.linalg.norm(a-y) ** 2
+
+    def delta_L(a,y,z):
+        return (a-y)*sigmoid_derivative(z)
+
+class CrossEntropyCost(object):
+    @staticmethod
+    def cost(a,y):
+        return np.sum(np.nan_to_num(-y*np.log(a)-(1-y)*np.log(1-a)))
+
+    @staticmethod
+    def delta_L(a,y,z):
+        return a-y
+
 class FullyConnectedNN(object):
-    def __init__(self, sizes):
+    def __init__(self, sizes,cost=CrossEntropyCost):
         self.num_layers = len(sizes)
         self.sizes = sizes
-        self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
-        self.weights = [np.random.randn(y, x)
-                        for x, y in zip(sizes[:-1], sizes[1:])]
+        self.cost_function = cost
+        self.init_weights()
+
+    def init_weights(self):
+        self.biases = [np.zeros(bias,1) for bias in self.sizes]
+        self.weights = [np.random.randn(i,j)/np.sqrt(j) for i,j in zip(sizes[:-1],sizes[1:])]
 
     def feedforward(self, a):
         """Return the output of the network if ``a`` is input."""
@@ -15,14 +35,15 @@ class FullyConnectedNN(object):
             a = sigmoid(np.dot(w, a)+b)
         return a
 
-    def SGD(self,training_data, batch_size, epochs,test_data=None):
-        #TODO: maybe add a loop over epochs here?
-        training_data = random.shuffle(training_data)
-        for k in range(0,len(training_data),batch_size):
-            mini_batch = training_data[k:k+batch_size]
-            self.update_mini_batch(mini_batch)
-            for i in range(len(self.weights)):
-                print(list(zip(self.weights,self.biases)))
+    def SGD(self,training_data,learning_rate,batch_size,epochs,test_data=None):
+        for i in range(epochs):
+            print("Epoch " + str(i))
+            training_data = random.shuffle(training_data)
+
+            for k in range(0,len(training_data),batch_size):
+                mini_batch = training_data[k:k+batch_size]
+                self.update_mini_batch(mini_batch,learning_rate)
+
 
     def update_mini_batch(self, mini_batch, learning_rate):
         biases = [np.zeros(b.shape) for b in self.biases]
@@ -32,8 +53,10 @@ class FullyConnectedNN(object):
             updated_b, updated_w = self.backpropagation(x,y)
             biases = [b+upd_b for b,upd_b in zip(biases,updated_b)]
             weights = [w+upd_w for w,upd_w in zip(weights,updated_w)]
+
         self.weights = [weight - (learning_rate/len(mini_batch))*upd_w \
         for weight,upd_w in zip(self.weights, weights)]
+
         self.biases = [bias - (learning_rate/len(mini_batch))*upd_b \
         for bias,upd_b in zip(self.biases,biases)]
 
@@ -48,8 +71,7 @@ class FullyConnectedNN(object):
             a = self.sigmoid(z)
             a_values.append(a)
             z_values.append(z)
-        delta_L = self.cost_derivative(a_values[-1]) * \
-        self.sigmoid_derivative(z_values[-1])
+        delta_L = (self.cost).delta_L(a_values[-1],y,z_values[-1])
 
         for j in range(len(updated_weights)-1,-1,-1):
             updated_biases[j] = delta_L
@@ -63,6 +85,3 @@ class FullyConnectedNN(object):
 
     def sigmoid_derivative(self,z):
         return self.sigmoid(z) * (1-self.sigmoid(z))
-
-
-nn = FullyConnectedNN([2,3])
